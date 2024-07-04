903917eb9f077417f04965596abbfd33eee373fb
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2024-07-04 17:23:59 +0700

    update: migrated to supabase

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.exc import IntegrityError
 from datetime import datetime
 import os
 import logging
 import json
-# import dotenv
+import dotenv
+from supabase import create_client, Client
 from functools import wraps
 from scripts.metadata import extract_metadata
-from sqlalchemy.orm import Session, sessionmaker
 
-# dotenv.load_dotenv()
+dotenv.load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
+supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
 
 def require_api_key(f):
     @wraps(f)
@@ -28,38 +27,11 @@ def require_api_key(f):
     return decorated_function
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:////tmp/data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 logger = logging.getLogger(__name__)
 
 with open('./data/sectors_data.json', 'r') as f:
     sectors_data = json.load(f)
-
-class Article(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    body = db.Column(db.Text, nullable=False)
-    source = db.Column(db.String(255), nullable=False)
-    timestamp = db.Column(db.DateTime, nullable=False)
-    sector = db.Column(db.String(100), nullable=False)
-    subsector = db.Column(db.String(100), nullable=False)
-    tags = db.Column(db.JSON, nullable=False)  # Array of String
-    tickers = db.Column(db.JSON, nullable=False)
-
-class LogEntry(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.DateTime, default=datetime)
-    level = db.Column(db.String(50))
-    message = db.Column(db.Text)
-    request_method = db.Column(db.String(10))
-    request_url = db.Column(db.String(255))
-    remote_addr = db.Column(db.String(50))
-
-with app.app_context():
-    SessionFactory = sessionmaker(bind=db.engine)
-    db.create_all()
 
 def sanitize_and_insert(data):
     # Sanitization v1.0
@@ -68,8 +40,8 @@ def sanitize_and_insert(data):
     source = data.get('source').strip()
     timestamp_str = data.get('timestamp').strip()
     timestamp = datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S')
-    subsector = data.get('subsector').strip()
-    sector = sectors_data[subsector] if subsector in sectors_data.keys() else ""
+    sub_sector = data.get('sub_sector').strip() if data.get('sub_sector') else data.get('subsector').strip()
+    sector = sectors_data[sub_sector] if sub_sector in sectors_data.keys() else ""
     tags = data.get('tags', []) 
     tickers = data.get('tickers', [])
     
@@ -87,38 +59,37 @@ def sanitize_and_insert(data):
         if body == '':
             body = generated_body
 
-    new_article = Article(
-        title=title,
-        body=body,
-        source=source,
-        timestamp=timestamp,
-        sector=sector,
-        subsector=subsector,
-        tags=tags,
-        tickers=tickers
-    )
+    new_article = {
+        'title': title,
+        'body': body,
+        'source': source,
+        'timestamp': timestamp.isoformat(),
+        'sector': sector,
+        'sub_sector': sub_sector,
+        'tags': tags,
+        'tickers': tickers
+    }
 
     try:
-        db.session.add(new_article)
-        db.session.commit()
-        return {"status": "success", "id": new_article.id}
-    except IntegrityError:
-        db.session.rollback()
-        return {"status": "error", "message": "Integrity error, possibly duplicate data"}
+        response = supabase.table('idx_news').insert(new_article).execute()
+        return {"status": "success", "id": response.data[0]['id']}
+    except Exception as e:
+        return {"status": "error", "message": f"Insert failed! Exception: {e}"}
+
 
 def log_request_info(level, message):
-    log_entry = LogEntry(
-        level=level,
-        message=message,
-        request_method=request.method,
-        request_url=request.url,
-        remote_addr=request.remote_addr,
-        timestamp=datetime.now()
-    )
-    db.session.add(log_entry)
-    db.session.commit()
-    
-    # logger.log(level, f"{message} - Method: {request.method}, URL: {request.url}, Remote Addr: {request.remote_addr}, Body: {request.data.decode('utf-8') if request.data else ''}")
+    log_entry = {
+        'timestamp': datetime.now().isoformat(),
+        'level': level,
+        'message': message,
+        'request_method': request.method,
+        'request_url': request.url,
+        'remote_addr': request.remote_addr
+    }
+    try:
+        supabase.table('idx_news_logs').insert(log_entry).execute()
+    except Exception as e:
+        print("Failed to insert log")
 
 @app.route('/articles', methods=['POST'])
 @require_api_key
@@ -142,19 +113,11 @@ def add_articles():
 @app.route('/articles', methods=['GET'])
 def get_articles():
     log_request_info(logging.INFO, 'Received GET request to /articles')
-    articles = Article.query.all()
-    articles_list = [{
-        'id': article.id,
-        'title': article.title,
-        'body': article.body,
-        'source': article.source,
-        'timestamp': article.timestamp.strftime('%Y-%m-%d %H:%M:%S'),
-        'sector': article.sector,
-        'subsector': article.subsector,
-        'tags': article.tags,  
-        'tickers': article.tickers
-    } for article in articles]
-    return jsonify(articles_list)
+    try:
+        response = supabase.table('idx_news').select('*').execute()
+        return jsonify(response.data)
+    except Exception as e:
+        return jsonify({"status": "error", "message": {e.message}})
 
 @app.route('/articles', methods=['DELETE'])
 @require_api_key
@@ -162,37 +125,23 @@ def delete_article():
     input_data = request.get_json()
     id_list = input_data.get('id_list')
     log_request_info(logging.INFO, f'Received DELETE request to /articles')
-    session = SessionFactory()
     list_result = []
     for id in id_list:
-        article = session.get(Article, id)
-        if article is None:
-            list_result.append({"status": "error", "message": "Article not found"})
-        else:
-            try: 
-                list_result.append({"status": "success", "message": f"Article with id {id} deleted"})
-                session.delete(article)
-                session.commit()
-            except Exception as e:
-                session.rollback()
-                list_result.append({"status": "error", "message": f"article cannot be deleted {e}"})
-    session.close()            
-    return list_result
+        try:
+            supabase.table('idx_news').delete().eq('id', id).execute()
+            list_result.append({"status": "success", "message": f"Article with id {id} deleted"})
+        except Exception as e:
+            list_result.append({"status": "error", "message": f"Error deleting article with id {id}: {e}"})
+    return jsonify(list_result)         
 
 @app.route('/logs', methods=['GET'])
 @require_api_key
 def get_logs():
-    logs = LogEntry.query.order_by(LogEntry.timestamp.desc()).all()
-    logs_list = [{
-        'id': log.id,
-        'timestamp': log.timestamp.strftime('%Y-%m-%d %H:%M:%S'),
-        'level': log.level,
-        'message': log.message,
-        'request_method': log.request_method,
-        'request_url': log.request_url,
-        'remote_addr': log.remote_addr
-    } for log in logs]
-    return jsonify(logs_list)
+    try:
+        response = supabase.table('idx_news_logs').select('*').order('timestamp', desc=True).execute()
+        return jsonify(response.data)
+    except Exception as e:
+        return jsonify({"status": "error", "message": e}), 500
 
 if __name__ == '__main__':
     app.run(debug=False)
