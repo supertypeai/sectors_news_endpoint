e9ba01010ecb155249ab31d2a07270617fa8fd75
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2024-07-05 14:52:04 +0700

    add: additional information on purpose

--- scripts/generate_article.py
+++ scripts/generate_article.py
@@ -22,24 +22,25 @@ def extract_info(text):
   for i, line in enumerate(lines):
     if "Nomor Surat" in line:
       article_info["document_number"] = lines[i-1]
-    elif "Nama Perusahaan" in line:
+    if "Nama Perusahaan" in line:
       article_info["company_name"] = lines[i-1]
-    elif "Nama Pemegang Saham" in line:
+    if "Nama Pemegang Saham" in line:
       article_info["shareholder_name"] = ' '.join(line.split()[3:])
-    elif "Kode Emiten" in line:
+    if "Kode Emiten" in line:
       article_info["ticker"] = lines[i-1]
-    elif "Kategori" in line:
+    if "Kategori" in line:
       article_info["category"] = ' '.join(line.split()[1:])
-    elif "Status Pengedali" in line or "Status Pengendali" in line:
+    if "Status Pengedali" in line or "Status Pengendali" in line:
       article_info["control_status"] = ' '.join(line.split()[2:])
-    elif "Jumlah Saham Sebelum Transaksi" in line:
+    if "Jumlah Saham Sebelum Transaksi" in line:
       article_info["shareholding_before"] = line.split()[-1]
-    elif "Jumlah Saham Setelah Transaksi" in line:
+    if "Jumlah Saham Setelah Transaksi" in line:
       article_info["shareholding_after"] = line.split()[-1]
     if "Tujuan Transaksi" in line:
       word = get_first_word(line.split()[2]).lower()
-      article_info["purpose"] = word if word == 'investasi (penambahan aset)' or word == 'divestasi (pengurangan aset)' else 'lainnya'
-    elif "Tanggal dan Waktu" in line or "Date and Time" in line:
+      article_info["purpose"] = word if word == 'investasi' or word == 'divestasi' else 'lainnya'
+      article_info["purpose"] += "(penambahan aset)" if word == 'investasi' else "(pengurangan aset)" if word == 'divestasi' else ""
+    if "Tanggal dan Waktu" in line or "Date and Time" in line:
       article_info["date_time"] = ' '.join(line.split()[3:])
   return article_info
 
